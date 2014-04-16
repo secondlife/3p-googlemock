602372c94371e2aaa4de51ec0c8003cba74e61dd
Monty Brandenberg <monty@lindenlab.com>
2014-04-16 21:54:20 +0000

    All:  Update Boost dependency though I think this is uneeded now. Select compilers/SDKs on Linux and Mac.  Debug build on Mac and add to manifest.  Update documentation a bit.  Reverse build order, debug before release, so any generated headers are of release flavor.

--- build-cmd.sh
+++ build-cmd.sh
@@ -29,66 +29,139 @@ pushd "$SOURCE_DIR"
 
         "windows")
             pushd msvc/2010
-            load_vsvars
-            build_sln "$PROJECT.sln" "Debug|Win32"
-            build_sln "$PROJECT.sln" "Release|Win32"
+                load_vsvars
+                build_sln "$PROJECT.sln" "Debug|Win32"
+                build_sln "$PROJECT.sln" "Release|Win32"
 
-            mkdir -p "$stage/lib/debug"
-            mkdir -p "$stage/lib/release"
+                mkdir -p "$stage/lib/debug"
+                mkdir -p "$stage/lib/release"
 
-            cp Release/*\.lib $stage/lib/release/
-            cp Debug/*\.lib $stage/lib/debug/
+                cp -a Release/*\.lib $stage/lib/release/
+                cp -a Debug/*\.lib $stage/lib/debug/
 
-            # copy headers
-            mkdir -p "$stage/include/$PROJECT"
-            mkdir -p "$stage/include/gtest"
+                # copy headers
+                mkdir -p "$stage/include/$PROJECT"
+                mkdir -p "$stage/include/gtest"
             popd
-            cp -rv include "$stage/"
-            cp -rv gtest/include "$stage/"
-
+            cp -a include "$stage/"
+            cp -a gtest/include "$stage/"
         ;;
 
         "darwin")
             # TODO: fix the mac build
-            CPPFLAGS="-DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" ./configure --prefix="$stage"
+
+            # Select SDK with full path.  This shouldn't have much effect on this
+            # build but adding to establish a consistent pattern.
+            #
+            # sdk=/Developer/SDKs/MacOSX10.6.sdk/
+            # sdk=/Developer/SDKs/MacOSX10.7.sdk/
+            # sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk/
+            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk/
+
+            opts="${TARGET_OPTS:--arch i386 -iwithsysroot $sdk -mmacosx-version-min=10.6}"
+
+            # Debug first
+            CPPFLAGS="-DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" \
+                CFLAGS="$opts -O0 -gdwarf-2" \
+                CXXFLAGS="$opts -O0 -gdwarf-2" \
+                LDFLAGS="-L$stage/packages/lib/debug" \
+                ./configure --prefix="$stage" --libdir="$stage"/lib/debug
             make
             make install
+
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
                 make check
             fi
-            
-            mv "$stage/lib" "$stage/release"
-            mkdir -p "$stage/lib"
-            mv "$stage/release" "$stage/lib"
-        ;;
 
-        "linux")
-            # Prefer 4.6
-            export CC=gcc-4.6
-            export CXX=g++-4.6
+            make distclean
 
-            CPPFLAGS="-DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" CFLAGS="-m32 -O2" CXXFLAGS="-m32" ./configure --prefix="$stage" --libdir="$stage/lib/release"
+            # Release last
+            CPPFLAGS="-DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" \
+                CFLAGS="$opts -gdwarf-2" \
+                CXXFLAGS="$opts -gdwarf-2" \
+                LDFLAGS="-L$stage/packages/lib/release" \
+                ./configure --prefix="$stage" --libdir="$stage"/lib/release
             make
             make install
+
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
                 make check
             fi
             
             make distclean
-            CPPFLAGS="-DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" CFLAGS="-m32 -O0 -g" CXXFLAGS="-m32" ./configure --prefix="$stage" --libdir="$stage/lib/debug"
+        ;;
+
+        "linux")
+            # Linux build environment at Linden comes pre-polluted with stuff that can
+            # seriously damage 3rd-party builds.  Environmental garbage you can expect
+            # includes:
+            #
+            #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
+            #    DISTCC_LOCATION            top            branch      CC
+            #    DISTCC_HOSTS               build_name     suffix      CXX
+            #    LSDISTCC_ARGS              repo           prefix      CFLAGS
+            #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
+            #
+            # So, clear out bits that shouldn't affect our configure-directed build
+            # but which do nonetheless.
+            #
+            # unset DISTCC_HOSTS CC CXX CFLAGS CPPFLAGS CXXFLAGS
+
+            # Prefer gcc-4.6 if available.
+            if [[ -x /usr/bin/gcc-4.6 && -x /usr/bin/g++-4.6 ]]; then
+                export CC=/usr/bin/gcc-4.6
+                export CXX=/usr/bin/g++-4.6
+            fi
+
+            # Default target to 32-bit
+            opts="${TARGET_OPTS:--m32}"
+
+            # Handle any deliberate platform targeting
+            if [ -z "$TARGET_CPPFLAGS" ]; then
+                # Remove sysroot contamination from build environment
+                unset CPPFLAGS
+            else
+                # Incorporate special pre-processing flags
+                export cppflags="$TARGET_CPPFLAGS"
+            fi
+
+            # Debug first
+            CPPFLAGS="${cppflags} -DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" \
+                CFLAGS="$opts -O0 -g" \
+                CXXFLAGS="$opts -O0 -g" \
+                LDFLAGS="-L$stage/packages/lib/debug" \
+                ./configure --prefix="$stage" --libdir="$stage"/lib/debug
             make
             make install
+
             # conditionally run unit tests
             if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
                 make check
             fi
             
+            make distclean
+
+            # Release last
+            CPPFLAGS="${cppflags} -DUSE_BOOST_TYPE_TRAITS -I$stage/packages/include" \
+                CFLAGS="$opts" \
+                CXXFLAGS="$opts" \
+                LDFLAGS="-L$stage/packages/lib/release" \
+                ./configure --prefix="$stage" --libdir="$stage"/lib/release
+            make
+            make install
+
+            # conditionally run unit tests
+            if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                make check
+            fi
+            
+            make distclean
         ;;
     esac
 
-	# copy license info
+    # copy license info
     mkdir -p "$stage/LICENSES"
     cp COPYING  "$stage/LICENSES/$PROJECT.txt"
 popd
